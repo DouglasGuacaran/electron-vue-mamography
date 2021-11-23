70abc6531e6cd7527cc325de4a390c502a64e525
DouglasGuacaran <douglasguacaran@gmail.com>
2021-11-23 10:49:00 -0300

    Algunas formas realizadas

--- src/views/Hoja1.vue
+++ src/views/Hoja1.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-      <b-row>
-          <b-col>
+      <div>
+          <div>
             <h1>Cualquier cosa 1</h1>
-          </b-col>
-      </b-row>
+            <form action="">
+              <label for="">Ingrese el nombre de la Institución:</label>
+              <input type="text">
+              <br>
+              <label for="">Ingrese la dirección de la Institución:</label>
+              <input type="text">
+              <br>
+            </form>
+            <button class="btn" type="submit">Prueba</button>
+          </div>
+      </div>
 
   </div>
 </template>
@@ -16,5 +25,7 @@ export default {
 </script>
 
 <style>
-
+.btn {
+  color: rgb(65, 180, 42);
+}
 </style>
